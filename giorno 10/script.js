917be2db26651330e094317bf0c1865d0936e123
LVin100 <vincent.legnani@gmail.com>
2022-12-15 09:55:57 +0100

    soluzione prima parte giorno 10

--- giorno 10/script.js
+++ giorno 10/script.js
@@ -142,147 +142,149 @@ noop
 noop
 noop
 noop`;
-let inputAOCTest = `addx 2
-addx 3
-noop
-addx 1
-addx 27
-addx -23
-addx 5
-noop
-addx 1
-noop
-addx 4
-addx 1
-noop
-addx 4
-addx 5
-noop
-noop
-noop
-addx 5
-addx -4
-addx 4
-noop
-addx 1
-addx -38
-noop
-noop
-addx 7
-addx 8
-addx -3
-noop
-addx 3
-noop
-addx 5
-noop
-noop
-addx -2
-addx 2
-addx 9
-addx -2
+let inputAOCTest = `addx 15
+addx -11
 addx 6
-addx 1
-addx -4
-addx 5
-addx 2
-addx -14
-addx -6
-addx -16
-addx 1
-addx 5
-addx 1
-addx 4
-addx -2
-noop
-addx -7
 addx -3
-addx 17
 addx 5
-noop
-noop
-addx 19
-addx -16
-noop
-addx 14
+addx -1
 addx -8
-addx 2
-noop
+addx 13
 addx 4
 noop
-addx -35
-addx -2
-noop
-noop
-addx 7
-addx 19
-addx -26
-addx 10
-addx 29
-addx -21
-noop
-addx 4
-noop
-noop
-addx -9
-addx 4
-addx 8
-addx 7
-noop
-addx -2
-addx 5
-addx 2
-addx -19
-addx -18
-noop
-noop
-noop
-noop
-addx 7
-addx -7
-addx 37
-addx -27
-addx 5
-addx 2
-addx -12
-addx 4
-addx 11
-noop
-noop
-noop
-addx 5
-addx -14
-addx 21
-addx -4
-addx 5
-addx 2
-noop
-addx -35
-noop
-noop
-noop
-noop
-addx 7
-addx 1
-noop
-noop
+addx -1
 addx 5
 addx -1
 addx 5
-addx 1
-noop
-addx 4
-addx 1
-noop
-noop
-addx 4
-noop
-addx 1
-addx 2
+addx -1
 addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
 addx 2
 addx 1
 noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
 noop
 noop
 noop`;
@@ -295,20 +297,33 @@ parsedInput.forEach((el, index) => {
 });
 
 console.log(instructions);
-let waiter = 0;
-let oldWaiter = 0;
-let veryOldWaiter = 0;
 let x = 1;
+let cycle = 0;
+let cycleToStamp = [20,60,100,140,180,220];
+let sum = 0;
 instructions.forEach(inst => {
 
     if(inst[0] === 'noop'){
-      x+=oldWaiter;
+      cycle++;
+      sum = isCycle20(cycle, sum, x);
     } else if(inst[0] === 'addx'){
-      waiter = inst[1];
-      x+= veryOldWaiter;
+      for(let i = 0; i<2; i++){
+        cycle++
+        sum = isCycle20(cycle, sum, x);
+      }
+      // console.log(x+' '+inst[1]);
+      x += inst[1];
     }
-    veryOldWaiter = oldWaiter;
-    oldWaiter = waiter;
+    
 })
 
-console.log(x);
+function isCycle20(cycle, sum, x){
+
+  if(cycleToStamp.includes(cycle)){
+    signalStrength = cycle*x;
+      sum += signalStrength;
+      console.log('al '+cycle+' ciclo la somma è: '+sum);
+  }
+  return sum;
+}
+console.log(sum);
